ee529acc4aa69a6e4b221b6a7d18bc02f837cc2e
Pierre Bellec <pierre.bellec@gmail.com>
2017-04-04 22:51:23 -0400

    Initial dashboard experiment.

---
+++ listMaps.js
@@ -0,0 +1,5 @@
+var listMaps = [ "del_minus_con" ,"del_minus_dup" ,"dup_minus_con" ];
+var params = {};
+params[0] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: -5.000000, max: 5.000000};
+params[1] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: -5.000000, max: 5.000000};
+params[2] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: -5.000000, max: 5.000000};
